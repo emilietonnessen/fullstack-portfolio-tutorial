d1153715046485f4264df559e3b9709002473f36
Emilie Tønnessen <emilie@tonnessen.com>
2022-12-07 18:29:00 +0100

    Add file and folder structure

--- frontend_react/src/App.tsx
+++ frontend_react/src/App.tsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import About from './container/About/About';
+import Footer from './container/Footer/Footer';
+import Header from './container/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Skills from './container/Skills/Skills';
+import Testimonial from './container/Testimonial/Testimonial';
+import Work from './container/Work/Work';
+import './App.scss';
 
 const App = () => {
-  return <h1>App</h1>;
+  return (
+    <div className="app">
+      <Navbar />
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
