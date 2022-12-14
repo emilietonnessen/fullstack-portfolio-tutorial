b7ced187646d05bd51e848244018f2b0a4f7fd3e
Emilie Tønnessen <emilie@tonnessen.com>
2022-12-14 13:09:22 +0100

    Add dynamic data to the about section

--- frontend_react/src/container/About/About.tsx
+++ frontend_react/src/container/About/About.tsx
@@ -2,55 +2,42 @@ import { motion } from 'framer-motion';
 
 import './About.scss';
 import images from '../../constants/images';
+import { useEffect, useState } from 'react';
+import { urlFor, client } from '../../client';
 
 const About = () => {
-  const abouts = [
-    {
-      title: 'Web Development',
-      description: ' I am a good web developer',
-      imgUrl: images.about01,
-    },
-    {
-      title: 'Web Design',
-      description: ' I am a good web designer',
-      imgUrl: images.about02,
-    },
-    {
-      title: 'React',
-      description: ' I am a good UI/UX designer',
-      imgUrl: images.about03,
-    },
-    {
-      title: 'TypeScript',
-      description: ' I am a good web animater',
-      imgUrl: images.about04,
-    },
-  ];
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]';
+
+    client.fetch(query).then((data) => {
+      setAbouts(data);
+    });
+  }, []);
 
   return (
     <>
       <h2 className="head-text">
-        I know that
-        <span> Good Development </span>
-        <br />
-        means
-        <span> Good Business</span>
+        I Know that <span>Good Design</span> <br />
+        means <span>Good Business</span>
       </h2>
+
       <div className="app__profiles">
-        {abouts.map((about, index) => (
+        {abouts.map((about: any, index) => (
           <motion.div
             whileInView={{ opacity: 1 }}
             whileHover={{ scale: 1.1 }}
             transition={{ duration: 0.5, type: 'tween' }}
             className="app__profile-item"
-            key={about.title + index}
+            key={about?.title + index}
           >
-            <img src={about.imgUrl} alt={about.title} />
+            <img src={urlFor(about?.imgUrl).url()} alt={about?.title} />
             <h2 className="bold-text" style={{ marginTop: 20 }}>
-              {about.title}
+              {about?.title}
             </h2>
-            <p className="bold-text" style={{ marginTop: 10 }}>
-              {about.description}
+            <p className="p-text" style={{ marginTop: 10 }}>
+              {about?.description}
             </p>
           </motion.div>
         ))}
